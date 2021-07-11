2f160342dbed6f381563556a8e0ceec38f1eae63
Alexander Sidin <sanasidin@mail.ru>
2021-07-11 15:07:38 +0300

    first edition

---
+++ academy/src/by/academy/homework1/Task_4.java
@@ -0,0 +1,25 @@
+package by.academy.homework1;
+
+
+
+public class Task_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double number = 2;
+		int i = 1;
+		
+		while (number <= 1_000_000) {
+			number = Math.pow(number, i);
+			System.out.println(number);
+			i++;
+		}
+		
+		
+			
+		}
+
+	
+
+}
