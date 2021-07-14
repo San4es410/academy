248d79ab2ecc5908fde151134edb650a7eb00b13
Alexander Sidin <sanasidin@mail.ru>
2021-07-14 13:06:34 +0300

    Third edition

--- academy/src/by/academy/homework1/Task_4.java
+++ academy/src/by/academy/homework1/Task_4.java
@@ -7,11 +7,11 @@ public class Task_4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		double number = 2;
-		int i = 1;
+		int number = 2;
+		int i = 0;
 		
 		while (number <= 1_000_000) {
-			number = Math.pow(2, i);
+			number = 2 << i;
 			System.out.println(number);
 			i++;
 		}
