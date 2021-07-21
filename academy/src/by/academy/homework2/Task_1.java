ebe2f320bc78bb0186bd310a7af0af346ee8e225
Alexander Sidin <sanasidin@mail.ru>
2021-07-21 20:39:08 +0300

    first version

---
+++ academy/src/by/academy/homework2/Task_1.java
@@ -0,0 +1,28 @@
+package by.academy.homework2;
+
+import java.util.Scanner;
+
+public class Task_1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String firstString = scan.nextLine();
+		String secondString = scan.nextLine();
+
+		System.out.println(compare(firstString, secondString));
+	}
+
+	public static boolean compare(String firstString, String secondString) {
+		if (firstString.length() != secondString.length()) {
+			return false;
+		}
+
+		int count = 0;
+		for (int i = 0; i < firstString.length(); i++) {
+			count ^= firstString.charAt(i) ^ secondString.charAt(i);
+		}
+		return count == 0;
+
+	}
+
+}
