71c91e1f7d3759db04f42ec38ff4a80e3443c24f
Alexander Sidin <sanasidin@mail.ru>
2021-08-15 16:08:22 +0300

    Task 2 and Task 3

---
+++ academy/src/by/academy/homework4/task_2/MyArrayTest.java
@@ -0,0 +1,35 @@
+package by.academy.homework4.task_2;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MyArrayTest {
+
+	public static void main(String[] args) {
+		MyArray<Integer> array = new MyArray<Integer>();
+		
+		array.add(1);
+		array.add(4);
+		array.add(5);
+		array.add(3);
+		array.add(8);
+		array.add(10);
+		array.add(7);
+		System.out.println(Arrays.toString(array.getArray()));
+		System.out.println(array.get(4));
+		System.out.println(array.getLast());
+		System.out.println(array.getFirst());
+		System.out.println(array.getSize());
+		System.out.println(array.getlastIndex());
+		array.removeIndex(4);
+		System.out.println(Arrays.toString(array.getArray()));
+		array.removeObject(4);
+		System.out.println(Arrays.toString(array.getArray()));
+		
+		MyIterator<Integer> iterator = array.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+	}
+
+}
