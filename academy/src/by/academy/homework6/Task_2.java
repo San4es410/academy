d54868202efadb503e72424c8d8a47f22a032bd8
Alexander Sidin <sanasidin@mail.ru>
2021-08-29 16:58:35 +0300

    first version

---
+++ academy/src/by/academy/homework6/Task_2.java
@@ -0,0 +1,41 @@
+package by.academy.homework6;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Task_2 {
+
+	public static void main(String[] args) {
+		String pathIn = "D:\\Files\\task2\\task.txt";
+		String pathOut = "D:\\Files\\task2\\result.txt";
+		File file = new File(pathOut);
+
+		file.getParentFile().mkdirs();
+
+		try {
+			file.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try (FileReader reader = new FileReader(pathIn);
+				FileWriter writer = new FileWriter(pathOut);
+				BufferedReader bufferReader = new BufferedReader(reader);
+				BufferedWriter bufferWriter = new BufferedWriter(writer)) {
+
+			while (bufferReader.ready()) {
+				String line = bufferReader.readLine();
+				bufferWriter.write(line);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
